025a42bb000b6ca32e6108f5f0de0130d6acacb3
waililo2033 <waililo2033@gmail.com>
2026-06-03 12:29:02 +0100

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: 20, fontSize: 20 }}>
+      <h1>Foot Pro 27 ⚽</h1>
+      <p>Site en cours de réparation...</p>
+    </div>
+  );
+}
